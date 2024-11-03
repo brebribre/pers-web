e8120710e0eb9036b509c4d5fe7c1f3674ca0b49
brebribre <70563428+brebribre@users.noreply.github.com>
2024-11-03 18:32:36 +0100

    change theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import RightContent from '@/pages/components/layout/RightContent';
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-1 xl:grid-cols-2 bg-gradient-to-t from-amber-700 via-red-700 to-purple-900">
+    <div className="grid grid-cols-1 lg:grid-cols-2 bg-gradient-to-t from-amber-700 via-red-700 to-purple-900">
         <Head>
             <title>Bryan Alvin | Web Developer</title>
         </Head>
